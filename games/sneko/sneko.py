336044385cf2d50c30faa92d4043798d4f21f8ce
EmilioMartinez <emiliocmartinez@gmail.com>
2024-07-10 21:34:13 -0300

    Refactor sneko into sneko.py

--- games/sneko/sneko.py
+++ games/sneko/sneko.py
@@ -5,36 +5,36 @@ from games.sneko.map import MapContent
 import uasyncio # type: ignore
 
 
+initial_time_step = 0.150
 time_decrease = 0.005
 min_time_step = 0.050
+
 starting_segments = [(6,7), (7,7), (8,7), (9,7)]
 
 
 class Sneko:
     def __init__(self, device):
         self.device = device
-        self.map = Map(device.graphics)
-        self.snake = Snake(device, self, starting_segments)
-        self.score = 0
-        self.time_step = 0.150
 
-    def step_time_wait(self):
-        return uasyncio.sleep(self.time_step)
-    
     async def setup(self):
-        await self.step_time_wait()
+        self.map = Map(self.device.graphics)
+        self.snake = Snake(self.device, self, starting_segments)
+        self.score = 0
+        self.time_step = initial_time_step
+
+        await uasyncio.sleep(initial_time_step)
         for i in range(4):
             self.device.buzzer.chirp()
             self.map.write(starting_segments[i], i)
-            await self.step_time_wait()
+            await uasyncio.sleep(initial_time_step)
         for i in range(8):
             self.device.buzzer.chorp()
             self.map.write((4 + i,8), MapContent.WALL)
             await uasyncio.sleep(0.1)
-        await self.step_time_wait()
+        await uasyncio.sleep(initial_time_step)
         self.device.buzzer.chorp()
         self.map.drop_eggplant()
-        await self.step_time_wait()
+        await uasyncio.sleep(initial_time_step)
 
     async def death(self, nextHead):
         await self.snake.die(nextHead)
@@ -42,25 +42,42 @@ class Sneko:
         deathSound = uasyncio.create_task(self.device.buzzer.play_death_tune())
         await self.device.graphics.draw_bmp_coroutine('/games/sneko/res/deathscreen.bmp', (0, 0))
         await deathSound
+
         await uasyncio.sleep(1)
+        self.device.graphics.clear_screen()
+        await uasyncio.sleep(0.5)
+        
+        self.device.graphics.write_text((40, 64), "Score: " + str(self.score), TFT.WHITE)
+        highscore_music = uasyncio.create_task(self.device.buzzer.play_highscores_tune())
+
+        while True:
+            await uasyncio.sleep(0.01)
+            x, y, button = self.device.joystick.read_joystick()
+            if (button):
+                break
+        
+        highscore_music.cancel()
+        self.device.graphics.clear_screen()
+        await uasyncio.sleep(0.5)
 
     async def run_game(self):
-        await self.setup()
         while True:
-            await self.step_time_wait()
-            head = self.snake.step()
-            if head is not None:
-                map_next_head = self.map.read(head)
-                if map_next_head == MapContent.EMPTY or head == self.snake.segments[0]:
-                    self.snake.retract_tail()
-                    self.snake.advance_head(head)
-                elif map_next_head == MapContent.EGGPLANT:
-                    self.snake.advance_head(head)
-                    self.device.buzzer.play_eat_sound()
-                    self.map.drop_eggplant()
-                    self.time_step = max(self.time_step - time_decrease, min_time_step)
-                    self.score += 1
-                else:
-                    await self.death(head)
-                    break
+            await self.setup()
+            while True:
+                await uasyncio.sleep(self.time_step)
+                head = self.snake.step()
+                if head is not None:
+                    map_next_head = self.map.read(head)
+                    if map_next_head == MapContent.EMPTY or head == self.snake.segments[0]:
+                        self.snake.retract_tail()
+                        self.snake.advance_head(head)
+                    elif map_next_head == MapContent.EGGPLANT:
+                        self.snake.advance_head(head)
+                        self.device.buzzer.play_eat_sound()
+                        self.map.drop_eggplant()
+                        self.time_step = max(self.time_step - time_decrease, min_time_step)
+                        self.score += 1
+                    else:
+                        await self.death(head)
+                        break
 
--- main.py
+++ main.py
@@ -4,24 +4,11 @@ from device.device import Device
 import uasyncio # type: ignore
 
 
-async def waitForResetButton(device):
-    while True:
-        await uasyncio.sleep(0.01)
-        x, y, button = device.joystick.read_joystick()
-        if (button):
-            break
-
 async def main():
     device = Device()
-    while True:
-        sneko = Sneko(device)
-        await sneko.run_game()
-        device.graphics.clear_screen()
-        await uasyncio.sleep(0.5)
-        device.graphics.write_text((40, 64), "Score: " + str(sneko.score), TFT.WHITE)
-        highscores_music = uasyncio.create_task(device.buzzer.play_highscores_tune())
-        await waitForResetButton(device)
-        highscores_music.cancel()
+
+    sneko = Sneko(device)
+    await sneko.run_game()
 
 
 uasyncio.run(main())
