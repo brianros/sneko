0d8f3003c6b24c9bd890849093a14777e661ac2d
EmilioMartinez <emiliocmartinez@gmail.com>
2024-11-06 19:35:51 -0300

    clear debug

--- main.py
+++ main.py
@@ -6,15 +6,10 @@ async def main():
     await apps.loader()
 
 try:
-    print("Main loop initiated")
     main_loop = main()
     uasyncio.run(main_loop)
 except KeyboardInterrupt:
-    # Here go debug methods
-    # Here go pre-termination methods
     main_loop.close()
 finally:
-    # Here go termination methods
-    print("Main loop terminated")
     device.reset()
 
