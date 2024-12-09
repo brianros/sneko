8a22939f6e36e62b133ebd7fda0e03bd267bec4f
Emilio Martinez <emiliocmartinez@gmail.com>
2024-12-09 08:51:13 -0300

    Create config file

---
+++ config.py
@@ -0,0 +1,20 @@
+
+
+joystick = {
+		"pin_x"				: 26
+	,	"pin_y"				: 27
+	,	"pin_button"		: 0
+}
+
+buzzer = {
+		"pin"				: 1
+}
+
+display = {
+		"spi_sck"			: 14
+	,	"spi_mosi"			: 15
+	,	"tft_aDC"			: 16
+	,	"tft_aReset"		: 17
+	,	"tft_aCS"			: 18
+}
+
--- device.py
+++ device.py
@@ -2,12 +2,13 @@ from peripherals import reset_all
 from peripherals.graphics.ST7735_controller import ST7735_Controller
 from peripherals.audio.buzzer import Buzzer
 from peripherals.joystick import Joystick
+import config
 
 
 def reset():
     reset_all()
 
-joystick = Joystick(26, 27, 0)
-audio = Buzzer(1)
-display = ST7735_Controller(14, 15, 16, 17, 18)
+joystick = Joystick(**config.joystick)
+audio = Buzzer(**config.buzzer)
+display = ST7735_Controller(**config.display)
 
